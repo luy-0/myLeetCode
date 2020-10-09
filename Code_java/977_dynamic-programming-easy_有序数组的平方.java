65d1acac922efbf9575c6ff70c0fa96e98084aca
Luy_0 <seven_ou@163.com>
2020-10-09 13:21:25 +0800

    笔记整理

--- Code_java/977_dynamic-programming-easy_有序数组的平方.java
+++ Code_java/977_dynamic-programming-easy_有序数组的平方.java
@@ -7,14 +7,43 @@ import java.util.Arrays;
  */
 
 // @lc code=start
+/* 解法1: 平方+排序 */
+// class Solution {
+//     public int[] sortedSquares(int[] A) {
+//         int len = A.length;
+//         int[] re = new int[len];
+//         for (int i = 0; i < len; i++) {
+//             re[i] = A[i]*A[i];
+//         }
+//         Arrays.sort(re);
+//         return re;
+//     }
+// }
+
+/* 解法2: 双指针 */
 class Solution {
     public int[] sortedSquares(int[] A) {
         int len = A.length;
         int[] re = new int[len];
-        for (int i = 0; i < len; i++) {
-            re[i] = A[i]*A[i];
+        int p = 0;
+        int q = len - 1;
+        int k = q;
+
+        while(A[p]<=0 && A[q]>0){
+            if(Math.abs(A[p]) > A[q]){
+                re[k--] = A[p] * A[p++];
+            }
+            else{
+                re[k--] = A[q] * A[q--];
+            }
         }
-        Arrays.sort(re);
+        while(p<=q && A[p]<=0){
+            re[k--] = A[p] * A[p++];
+        }
+        while(p<=q && A[q]>0){
+            re[k--] = A[q] * A[q--];
+        }
+
         return re;
     }
 }
