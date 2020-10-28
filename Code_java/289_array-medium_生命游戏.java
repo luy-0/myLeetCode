de5f3da2135a100e0f0465a2c6ee41823825f424
Luy_0 <seven_ou@163.com>
2020-10-28 21:17:06 +0800

    10.28

--- Code_java/289_array-medium_生命游戏.java
+++ Code_java/289_array-medium_生命游戏.java
@@ -6,9 +6,62 @@
 
 // @lc code=start
 class Solution {
-    public void gameOfLife(int[][] board) {
+    private int[][] board;
+    private int[] dx = { -1, -1, -1, 0, 0, 1, 1, 1 };
+    private int[] dy = { -1, 0, 1, -1, 1, -1, 0, 1 };
+    private int liveCells, rows, cols;
 
+    public void gameOfLife(int[][] board) {
+        /*
+         * 状态说明 0: 00 死->死 1: 01 活->死 2: 10 死->活 3: 11 活->活
+         */
+        this.board = board;
+        rows = board.length;
+        cols = board[0].length;
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                // 统计周围活细胞
+                liveCells = countCells(row, col);
+                // 更新当前状态
+                // 如果细胞死亡,则0还是0,1还是1,无需转换
+                if (liveCells < 2 || liveCells > 3) {
+                    continue;
+                }
+                // 如果当前细胞继续存活,1->3
+                if (board[row][col] == 1 && (liveCells == 2 || liveCells == 3)) {
+                    board[row][col] = 3;
+                }
+                // 如果复活,0->2
+                if (board[row][col] == 0 && liveCells == 3) {
+                    board[row][col] = 2;
+                }
+            }
+        }
+
+        // 最后处理列表,01死23活
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                board[row][col] /= 2;
+            }
+        }
+
+    }
+
+    private int countCells(int row, int col) {
+        int count = 0;
+        for (int i = 0; i < 8; i++) {
+            int nx = row + dx[i];
+            int ny = col + dy[i];
+            if (nx < 0 || nx >= rows || ny < 0 || ny >= cols) {
+                continue;
+            }
+            // 状态为1或3时计数
+            if (board[nx][ny] % 2 == 1) {
+                count++;
+            }
+        }
+        return count;
     }
 }
 // @lc code=end
-
--- Code_java/42_array-hard_接雨水.java
+++ Code_java/42_array-hard_接雨水.java
@@ -19,32 +19,28 @@ class Solution {
 
         leftMaxVol[0] = height[0];
         for (int i = 1; i < height.length; i++) {
-            leftMaxVol[i] = Math.max(leftMaxVol[i-1], height[i]);
+            leftMaxVol[i] = Math.max(leftMaxVol[i - 1], height[i]);
         }
 
-        rightMaxVol[n-1] = height[n-1];
-        for (int i = n-2; i >=0; i--) {
-            rightMaxVol[i] = Math.max(rightMaxVol[i+1], height[i]);
+        rightMaxVol[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMaxVol[i] = Math.max(rightMaxVol[i + 1], height[i]);
         }
 
         // 用于打印的两个数组,以便纠错
-        /* for (int i = 0; i < leftMaxVol.length; i++) {
-            System.out.printf("%d,", leftMaxVol[i]);
-        }
-        System.out.println();
+        /*
+         * for (int i = 0; i < leftMaxVol.length; i++) { System.out.printf("%d,",
+         * leftMaxVol[i]); } System.out.println();
+         * 
+         * for (int i = 0; i < rightMaxVol.length; i++) { System.out.printf("%d,",
+         * rightMaxVol[i]); } System.out.println();
+         */
 
-        for (int i = 0; i < rightMaxVol.length; i++) {
-            System.out.printf("%d,", rightMaxVol[i]);
-        }
-        System.out.println(); */
+        for (int i = 1; i < height.length - 1; i++) {
+            int leftHeight = Math.min(leftMaxVol[i - 1], rightMaxVol[i - 1]);
+            int rightHeight = Math.min(leftMaxVol[i + 1], rightMaxVol[i + 1]);
 
-        
-
-        for (int i = 1; i < height.length-1; i++) {
-            int leftHeight = Math.min(leftMaxVol[i-1], rightMaxVol[i-1]);
-            int rightHeight = Math.min(leftMaxVol[i+1], rightMaxVol[i+1]);
-
-            if (leftHeight<=height[i] || rightHeight<= height[i]) {
+            if (leftHeight <= height[i] || rightHeight <= height[i]) {
                 continue;
             }
             ans += Math.min(leftHeight, rightHeight) - height[i];
